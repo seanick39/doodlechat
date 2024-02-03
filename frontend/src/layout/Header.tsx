2a3bb741b86d9adcd43af1c9de3cd97b1727347a
nikhil <nikhil.chawla@mailbox.org>
2024-02-03 18:06:53 +0100

    [frontend:css] finishing touches for header; doodle logo and user icon

--- frontend/src/layout/Header.tsx
+++ frontend/src/layout/Header.tsx
@@ -1,6 +1,9 @@
 import type React from "react";
 import type {User} from "../types";
 
+// @ts-ignore
+import {ReactComponent as UserIconSVG} from "../user.svg";
+
 type Props = {
   user: User | null;
 }
@@ -8,7 +11,14 @@ type Props = {
 export default function Header(props: Props): React.JSX.Element {
   return (
     <header>
-      <div id="header-username">{props.user?.name}</div>
+      <div id="header-container">
+        <div id="header-logo"><img src="/favicon.ico" alt="logo"/></div>
+        <div id="header-brand">Doodle Chat</div>
+        <div id="header-user">
+          <UserIconSVG/>
+          <div id="header-username">{props.user?.name}</div>
+        </div>
+      </div>
     </header>
   )
 }
